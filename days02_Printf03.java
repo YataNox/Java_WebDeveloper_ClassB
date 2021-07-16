fb4bff8f612005454804c97eeef15601dafcdaaf
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-16 20:42:49 +0900

    Update days02_Printf03.java

--- days02_Printf03.java
+++ days02_Printf03.java
@@ -13,11 +13,11 @@ public class Printf03
 		// 맞춰지게 출력)
 		System.out.println("\t\t  ###성적표###");
 		System.out.println("--------------------------------------------------");
-		System.out.printf("%s\t%-10s\t %2s\t %2s\t %2s\t %2s\t %2s\n", "번호", "성명", "국어", "영어", "수학", "총점", "평균");
+		System.out.printf("%3s\t%10s\t %3s\t %3s\t %3s\t %3s\t %3s\n", "번호", "성명", "국어", "영어", "수학", "총점", "평균");
 		System.out.println("--------------------------------------------------");
-		System.out.printf("1\t%-10s\t %2d\t %2d\t %2d\t %2d\t %2.1f\n", "홍길동", 85, 56, 78, 85+56+78, (85+56+78) / 3.0);
-		System.out.printf("2\t%-10s\t %2d\t %2d\t %2d\t %2d\t %2.1f\n", "Gildong" ,84, 87, 85, 84+87+85, (84+87+85) / 3.0);
-		System.out.printf("3\t%-10s\t %2d\t %2d\t %2d\t %2d\t %2.1f\n", "HongGil", 58, 87, 89, 58+87+89, (58+87+89) / 3.0);
+		System.out.printf("%3d\t%10s\t %3d\t %3d\t %3d\t %3d\t %3.1f\n", 1, "홍길동", 85, 56, 78, 85+56+78, (85+56+78) / 3.0);
+		System.out.printf("%3d\t%10s\t %3d\t %3d\t %3d\t %3d\t %3.1f\n", 2, "Gildong" ,84, 87, 85, 84+87+85, (84+87+85) / 3.0);
+		System.out.printf("%3d\t%10s\t %3d\t %3d\t %3d\t %3d\t %3.1f\n", 3, "HongGil", 58, 87, 89, 58+87+89, (58+87+89) / 3.0);
 		System.out.println("--------------------------------------------------");
 	}
 }
