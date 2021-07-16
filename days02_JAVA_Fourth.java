c42cb8f6c862cf9422128dddcb5640711a4f92da
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-16 17:17:50 +0900

    Create days02_JAVA_Fourth.java

---
+++ days02_JAVA_Fourth.java
@@ -0,0 +1,16 @@
+package days02;
+
+public class JAVA_Fourth 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\t\t  ###성적표###");
+		System.out.println("--------------------------------------------------");
+		System.out.println("번호\t성명\t\t국어\t 영어\t 수학\t 총점\t 평균");
+		System.out.println("--------------------------------------------------");
+		System.out.println("1\t홍길동\t\t 85\t 56\t 78\t 250\t 75.3");
+		System.out.println("2\tGildong\t 84\t 87\t 85\t 248\t 78.3");
+		System.out.println("3\tHongGil\t 58\t 87\t 89\t 278\t 86.6");
+		System.out.println("--------------------------------------------------");
+	}
+}
