92fc6392379a99b1ea80fdbcf137e8e427b9cba8
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-19 20:52:08 +0900

    Update days03_Variable05.java

--- days03_Variable05.java
+++ days03_Variable05.java
@@ -37,3 +37,46 @@ public class Variable05
 		sc.close();
 	}
 }
+
+/*
+package days03;
+
+import java.util.Scanner;
+
+public class Variable05 
+{
+	public static void main(String args[])
+	{
+		int kor, mat, eng, total;
+		double avg;
+		String name;
+		Scanner sc = new Scanner(System.in);
+		
+		// 이름, 국어, 영어, 수학 점수를 입력 받아서 1명의 성적표를 총점 평균과 함께 
+		// 출력하세요
+		
+		// 이름, 국어, 영어, 수학 성적 입력
+		System.out.printf("이름을 입력하세요 : ");
+		name = sc.nextLine();
+		System.out.printf("국어 성적을 입력하세요 : ");
+		kor = sc.nextInt();
+		System.out.printf("영어 성적을 입력하세요 : ");
+		eng = sc.nextInt();
+		System.out.printf("수학 성적을 입력하세요 : ");
+		mat = sc.nextInt();
+		
+		//총점, 평균 값 계산
+		total = kor + eng + mat;
+		avg = total / 3.0;
+		
+		//이름부터 평균까지의 값 출력
+		System.out.println("---------------------------------------------------");
+		System.out.printf("%s\t %s\t\t %s\t %s\t %s\t %s\t %s\n", "번호", "이름", "국어", "영어", "수학", "총점", "평균");
+		System.out.println("---------------------------------------------------");
+		System.out.printf("%d\t %s\t %d\t %d\t %d\t %d\t %.2f\n", 1, name, kor, eng, mat, total, avg);
+		System.out.println("---------------------------------------------------");
+		sc.close();
+	}
+}
+
+*/
