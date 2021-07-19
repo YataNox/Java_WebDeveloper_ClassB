7aaf84f0ddd5506162774a5472e2448215502d31
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-19 16:54:06 +0900

    Create days03_Variable01.java

---
+++ days03_Variable01.java
@@ -0,0 +1,18 @@
+package days03;
+
+public class Variable01 
+{
+	public static void main(String args[]) 
+	{
+		System.out.println(35 + "+" + 38 + "=" + (35+38));
+		System.out.println(35 + "-" + 38 + "=" + (35-38));
+		System.out.println(35 + "x" + 38 + "=" + (35*38));
+		System.out.println(35 + "/" + 38 + "=" + (35/38.0));
+		System.out.println(35 + "%" + 38 + "=" + (35%38));
+		
+		// 변수 : 프로그램 실행 중 발생 또는 입력되는 데이터를 저장할 목적으로 만들어서
+		// 사용하는 임시 저장 장소. 생성 명령 실행시 생성되고, 프로그램 종료시 소멸됩니다.
+		
+		
+	}
+}
