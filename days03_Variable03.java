ab0b24e5ab109172c2e1a7a666f3a30519de6f8e
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-19 19:53:33 +0900

    Update days03_Variable03.java

--- days03_Variable03.java
+++ days03_Variable03.java
@@ -14,7 +14,7 @@ public class Variable03
 		System.out.printf("높이를 입력하세요 : ");
 		int height = sc.nextInt();
 		
-		int area = (base * height) / 2; // 삼각형 넓이 공식 => 밑변 * 높이 / 2
+		double area = (base * height) / 2; // 삼각형 넓이 공식 => 밑변 * 높이 / 2
 		
 		System.out.println("입력한 삼각형의 넓이는 " + area + "입니다.");
 		sc.close();
