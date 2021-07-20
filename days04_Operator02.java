9d77000854417168c19216fac1e3b05f48100b92
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-20 18:10:12 +0900

    Create days04_Operator02.java

---
+++ days04_Operator02.java
@@ -0,0 +1,14 @@
+package days04;
+
+public class Operator02 
+{
+	public static void main(String args[])
+	{
+		// 연산에 사용할 변수 선언 및 값의 초기화
+		// 초기화 : 변수 생성 후 처음 값을 넣는 동작
+		int n = 50;
+		double d = 50.0;
+		
+		n = n + 10;
+	}
+}
