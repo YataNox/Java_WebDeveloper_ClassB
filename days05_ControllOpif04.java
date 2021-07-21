23e50813da75ff875943c05dcce7d4278b34ebe4
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-21 17:48:56 +0900

    Update days05_ControllOpif04.java

--- days05_ControllOpif04.java
+++ days05_ControllOpif04.java
@@ -15,14 +15,16 @@ public class ControllOpif04
 		// 12세 미만 어린이, 12~18세 청소년, 19~25세 청년
 		// 26세 이상 성년입니다. 라고 출력하는 if문 작성
 		
-		if(na < 12)
+		if(na >= 1 && na < 12)
 			System.out.printf("%d세, 어린이입니다.\n", na);
 		else if(na >=12 && na <= 18)
 			System.out.printf("%d세, 청소년입니다.\n", na);
 		else if(na >=19 && na <= 25)
 			System.out.printf("%d세, 청년입니다.\n", na);
-		else
+		else if(na >= 26)
 			System.out.printf("%d세, 성년입니다.\n", na);
+		else
+			System.out.printf("잘못입력하셨습니다.\n");
 		
 	}
 }
