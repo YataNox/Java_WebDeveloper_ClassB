fd8613201706303c18711e76d40e335bf0ae14ed
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-21 17:26:37 +0900

    Update days05_ControllOpif03.java

--- days05_ControllOpif03.java
+++ days05_ControllOpif03.java
@@ -11,10 +11,19 @@ public class ControllOpif03
 		int num = sc.nextInt();
 		
 		if(num > 0)
-			System.out.printf("입력한 정수 %d는 양수입니다.", num);
+			System.out.printf("입력한 정수 %d는 양수입니다.\n", num);
 		else if(num == 0)
-			System.out.printf("입력한 정수 %d는 0입니다.", num);
+			System.out.printf("입력한 정수 %d는 0입니다.\n", num);
 		else
-			System.out.printf("입력한 정수 %d는 음수입니다.", num);
+			System.out.printf("입력한 정수 %d는 음수입니다.\n", num);
+		
+		// 위에서 ()안의 연산식으로 계산한 겨로가가 true인 경우 {} 안의 명령이
+		// 실행됩니다. 다만, 그 명령이 단 한개만 존재한다면 {}는 생략할 수 있습니다.
+		// 중괄호 생략 후 if 구문과 실행 명령을 한 줄에 써도 무관합니다.
+		if(num > 0)System.out.printf("입력한 정수 %d는 양수입니다.\n", num);
+		else if(num == 0)System.out.printf("입력한 정수 %d는 0입니다.\n", num);
+		else System.out.printf("입력한 정수 %d는 음수입니다.\n", num);
+		
+		
 	}	
 }
