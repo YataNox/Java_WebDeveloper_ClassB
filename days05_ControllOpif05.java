2a5ffbcc21e75aa9f73827b7ed97d4126ab8bb74
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-21 18:26:38 +0900

    Update days05_ControllOpif05.java

--- days05_ControllOpif05.java
+++ days05_ControllOpif05.java
@@ -24,5 +24,28 @@ public class ControllOpif05
 			System.out.println("D학점");
 		else if(num < 60)
 			System.out.println("F학점");
+		else
+			System.out.println("잘못입력하셨습니다.");
+		// () > 산술연산 > 관계(비교)연산 > 논리연산
+		
+		if(num >= 90)
+			System.out.println("A학점");
+		else if(num >= 80)
+			System.out.println("B학점");
+		else if(num >= 70)
+			System.out.println("C학점");
+		else if(num >= 60)
+			System.out.println("D학점");
+		else
+			System.out.println("F학점");
+		
+		// if(){}, else if(){}에서 괄호() 안에는
+		// 해당 구문에 해당하는 명령{}을 실행할지, 건너뛰고 실행하지 않을지를
+		// 결정할 관계(비교) 연산식 또는 boolean 값이 들어갑니다.
+		// 만약 if문을 if(true){}와 같이 작성했다면
+		// {} 안의 명령은 ()안의 값이 true이므로 항상 실행됩니다.
+		// 반대로 if(false){}라면 항상 실행되지 않습니다.
+		// if, else if, else를 구성할 때는 각 구간들의 값들을 빠짐없이 챙겨서
+		// 구간을 구성하는 것이 중요합니다.
 	}
 }
