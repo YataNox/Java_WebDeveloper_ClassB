9616bb5422d9031ac212be4b28385cdc2554fb6c
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-22 18:45:01 +0900

    Create days06_ControllOpFor03.java

---
+++ days06_ControllOpFor03.java
@@ -0,0 +1,14 @@
+package days06;
+
+public class ControllOpFor03 
+{
+	public static void main(String argsp[])
+	{
+		// 1부터 10까지의 합
+		// int sum = 1+2+3+4+5+6+7+8+9+10;
+		int sum = 0;
+		for(int i = 1; i <= 10; i++)
+			sum += i;
+		System.out.println(sum);
+	}
+}
