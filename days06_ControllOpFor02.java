3ea670ee4af10e4958d4fe1ece0a067fdd649a20
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-22 18:35:26 +0900

    Update days06_ControllOpFor02.java

--- days06_ControllOpFor02.java
+++ days06_ControllOpFor02.java
@@ -20,6 +20,19 @@ public class ControllOpFor02
 		}
 		System.out.printf("\n 반복 실행후의 i변수 값 : %d\n", i);
 		System.out.println("\n 1부터 100사이의 짝수 출력 #2----------------");
+		for(i = 2; i <= 100; i+=2)
+		{
+			System.out.printf("%d ", i);
+		}
+		System.out.printf("\n 반복 실행후의 i변수 값 : %d\n", i);
+		
+		System.out.println("\n 1부터 100사이의 홀수 출력 #1----------------");
+		for(i = 1; i <= 100; i++)
+		{
+			if(i % 2 == 1) System.out.printf("%d ", i);
+		}
+		System.out.printf("\n 반복 실행후의 i변수 값 : %d\n", i);
+		System.out.println("\n 1부터 100사이의 홀수 출력 #2----------------");
 		for(i = 1; i <= 100; i+=2)
 		{
 			System.out.printf("%d ", i);
