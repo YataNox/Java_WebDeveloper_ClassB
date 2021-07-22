f62db0ee777524bcc2ecdbb005f636eecfd8032f
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-22 17:39:07 +0900

    Update days06_ControllOpQuestionMartk.java

--- days06_ControllOpQuestionMartk.java
+++ days06_ControllOpQuestionMartk.java
@@ -24,6 +24,19 @@ public class ControllOpQuestionMartk
 		prnText = (score >= 70)? "합격" : "불합격"; 
 		
 		System.out.println("입력한 점수는 " + prnText + "입니다.");
+		
+		System.out.printf("기본급을 입력하세요 : ");
+		int a = sc.nextInt();
+		// 기본급 100만원 이상이면 50%, 100만원 미만이면 60% 가산하여 지급액을
+		//출력
+		double per1 = (a >= 1000000)? 0.5 : 0.6;
+		System.out.println("총 지급액 : " + (int)(a + (a * per1)));
+		
+		double per2 = (a >= 1000000)? (a + (a * 0.5)) : (a + (a * 0.6));
+		System.out.println("총 지급액 : " + (int)per2);
+		
+		//(a >= 1000000)? System.out.println("총 지급액 : " + a+a*0.5); : System.out.println("총 지급액 : " + a+a*0.6); //에러
+		
 		sc.close();
 	}
 }
