52c279be16fcf80b8cbe0a207da84a1d290b29ea
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-22 19:59:59 +0900

    Update days06_ControllOpFor04.java

--- days06_ControllOpFor04.java
+++ days06_ControllOpFor04.java
@@ -19,5 +19,6 @@ public class ControllOpFor04
 		for(int num = 1; num<10; num++)
 			System.out.printf("%d x %d = %d\n", dan, num, dan*num);
 			//System.out.println(dan + "x" + num + "=" +dan*num);
+		sc.close();
 	}
 }
