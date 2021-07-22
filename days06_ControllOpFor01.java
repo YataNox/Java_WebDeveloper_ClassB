b97085ee76273be0c0ac2183d0b723d7ca0cd7f6
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-22 17:44:37 +0900

    Create days06_ControllOpFor01.java

---
+++ days06_ControllOpFor01.java
@@ -0,0 +1,17 @@
+package days06;
+
+public class ControllOpFor01 
+{
+	public static void main(String args[])
+	{
+		// 한 번 실행
+		System.out.println("겁나 쉬운 자바 프로그래밍~~!!");
+		
+		// for 명령 : 반복 실행 명령
+		for(int i = 0; i < 9; i++)
+		{
+			System.out.println("겁나 쉬운 자바 프로그래밍~~!!");
+		}
+		// 괄호 안에 ;으로 구분해서 세가지 내용을 기술해줍니다.
+	}
+}
