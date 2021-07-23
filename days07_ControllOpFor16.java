dbcfe37f4782260f5173f468900e413c32e402ca
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-23 18:48:18 +0900

    Update days07_ControllOpFor16.java

--- days07_ControllOpFor16.java
+++ days07_ControllOpFor16.java
@@ -6,8 +6,7 @@ public class ControllOpFor16
 	{
 		for(int i = 1; i <= 10; i++)
 		{
-			for(int j = 1; j <= 10; j++)
-				if(i >= j)
+			for(int j = 1; j <= i; j++)
 					System.out.printf("#");
 			System.out.println();
 		}
