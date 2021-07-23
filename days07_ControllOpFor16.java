054e4fd7bdcf0230aad6d485faf9693c732447d1
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-23 20:03:30 +0900

    Update days07_ControllOpFor16.java

--- days07_ControllOpFor16.java
+++ days07_ControllOpFor16.java
@@ -4,11 +4,49 @@ public class ControllOpFor16
 {
 	public static void main(String args[])
 	{
+		// 삼각형 1
 		for(int i = 1; i <= 10; i++)
 		{
 			for(int j = 1; j <= i; j++)
 					System.out.printf("#");
 			System.out.println();
 		}
+		System.out.println();
+		//-------------------------------------------------------------------------------
+		// 역삼각형 1
+		for(int i = 1; i <= 10; i++)
+		{
+			for(int j = 10; j >= i; j--)
+					System.out.printf("#");
+			System.out.println();
+		}
+		System.out.println();
+		//-------------------------------------------------------------------------------
+		// 역삼각형2
+		for(int i = 10; i >= 1; i--)
+		{
+			for(int j = 10; j >= 1; j--) 
+			{
+				if(i >= j)
+					System.out.printf("*");
+				else
+					System.out.printf(" ");
+			}
+			System.out.println();
+		}
+		
+		/*
+		 for(int i = 1; i <= 10; i++)
+		{
+			for(int j = 1; j <= 10; j++) 
+			{
+				if(j>=i)
+					System.out.printf("*");
+				else
+					System.out.printf(" ");
+			}
+			System.out.println();
+		}
+		 * */
 	}
 }
