382e8ff3812582ab6d9906660f569fbf537f6d82
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-23 18:43:14 +0900

    Create days07_ControllOpFor16.java

---
+++ days07_ControllOpFor16.java
@@ -0,0 +1,14 @@
+package days07;
+
+public class ControllOpFor16 
+{
+	public static void main(String args[])
+	{
+		for(int i = 1; i <= 10; i++)
+		{
+			for(int j = 1; j <= 10; j++)
+				System.out.printf("#");
+			System.out.println();
+		}
+	}
+}
