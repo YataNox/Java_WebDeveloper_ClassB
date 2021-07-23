009e52f38b36dd386c6314bf3ae5a0d8ede4680c
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-23 20:56:52 +0900

    Update days07_ControllOpFor17.java

--- days07_ControllOpFor17.java
+++ days07_ControllOpFor17.java
@@ -6,6 +6,8 @@ public class ControllOpFor17
 	{
 		// 1부터 100 사이의 소수(Prime Number)를 출력하세요.
 		int count = 0;
+		// 1부터 100까지의 숫자들을 1부터 자신까지의 숫자들 모두로 나눠보고
+		// 딱 떨어지는 숫자가 2개인 숫자들만 모아서 출력한다.
 		for(int i = 1; i <= 100; i++)
 		{
 			for(int j = 1; j <= i; j++)
@@ -20,8 +22,7 @@ public class ControllOpFor17
 			}
 			else
 			{
-				count = 
-						0;
+				count = 0;
 			}
 		}
 	}
