463182a62ef36a3232104448450bf1e53ed101c6
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-23 18:27:03 +0900

    Update days07_ControllOpFor15.java

--- days07_ControllOpFor15.java
+++ days07_ControllOpFor15.java
@@ -6,12 +6,17 @@ public class ControllOpFor15
 	{
 		//구구단 출력
 		
-		for(int i = 1; i <= 10; i++)
+		for(int i = 1; i <= 9; i++)
 		{
-			for(int j = 1; j <= 10; j++)
-			{
-				System.out.printf("%2d x %2d = %2d\t", j, i, j*i);
-			}
+			for(int j = 2; j <= 5; j++)
+				System.out.printf("%d x %d = %d\t\t", j, i, j*i);
+			System.out.println();
+		}
+		System.out.println();
+		for(int i = 1; i <= 9; i++)
+		{
+			for(int j = 6; j <= 9; j++)
+				System.out.printf("%d x %d = %d\t\t", j, i, j*i);
 			System.out.println();
 		}
 	}
