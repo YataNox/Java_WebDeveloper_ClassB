85647dc8ca81f05dae5eb8093c15396e625b39b7
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-27 18:37:56 +0900

    Update days09_Array08.java

--- days09_Array08.java
+++ days09_Array08.java
@@ -1,1 +1,50 @@
+package days09;
 
+import java.util.Random;
+
+public class Array08 
+{
+	public static void main(String[] args)
+	{
+		int[] a = new int[6];
+		int t;
+		
+		Random rd = new Random();
+		
+		a[0] = rd.nextInt(); // Int 범위내 랜덤 값 생성
+		
+		if(a[0] < 0) // 음수일 경우 양수로 변환
+			a[0] *= -1;
+		
+		// 랜덤 값 1~45범위로 조정, 출력
+		a[0] = a[0] % 45 + 1; // a[0] = (rd.nextInt() * 45) + 1;
+		System.out.printf("%d ", a[0]);
+		System.out.println();
+		
+		// 6개의 랜덤 값 생성
+		for(int i = 0; i < a.length; i++)
+		{
+			a[i] = rd.nextInt();
+	
+			if(a[i] < 0)
+				a[i] *= -1;
+			
+			a[i] = a[i] % 45 + 1; // a[i] = (rd.nextInt() * 45) + 1;
+		}
+		
+		// 오름차순 정렬
+		for(int i = 0; i < a.length; i++)
+			for(int j = i+1; j < a.length; j++)
+				if(a[i] > a[j])
+				{
+					t = a[i];
+					a[i] = a[j];
+					a[j] = t;
+				}
+		
+		// 출력
+		for(int i = 0; i < a.length; i++)
+			System.out.printf("%d ", a[i]);
+		
+	}
+}
