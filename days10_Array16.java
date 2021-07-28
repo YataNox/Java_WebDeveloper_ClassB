2e0f5dab453c7e2f68873e8aed9d1252fffd047a
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-28 17:41:07 +0900

    Update days10_Array16.java

--- days10_Array16.java
+++ days10_Array16.java
@@ -7,7 +7,7 @@ public class Array16
 		// 1차원 배열의 복사
 		int[] a = {1,2,3,4,5};
 		int[] b;
-		b = a; // b배열은 a의 주소를 복사
+		b = a; // b배열은 a의 주소를 복사, 즉 복사보단 공유의 개념
 		
 		for(int k : a)
 			System.out.printf("%d ", k);
@@ -24,9 +24,23 @@ public class Array16
 		
 		for(int k : b)
 			System.out.printf("%d ", k);
+		System.out.println();
 		// 레퍼런스 변수간의 값의 복사로는 배열의 복사까지 이루어지지 않습니다.
 		// 위의 동작 b = a;는 a가 갖고 있는 참조값(주소)를 b 변수에 복사한 것이므로
 		// 배열 공간은 하나, 그 배열 주소를 저장한 변수는 두 개로 설정한 것과 같습니다.
 		// 따라서 a[2]값 변경은 b[2]값 변경과 같습니다.
+		
+		b = new int[5];
+		for(int i = 0; i < a.length; i++)
+			b[i] = a[i];
+		
+		a[4] = 300;
+		for(int k : a)
+			System.out.printf("%d ", k);
+		System.out.println();
+		
+		for(int k : b)
+			System.out.printf("%d ", k);
+		System.out.println();
 	}
 }
