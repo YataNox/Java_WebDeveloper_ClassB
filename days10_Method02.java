2cc20fffb338bf8ff4aacf75cf2e30aa3ea0ec4d
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-28 19:46:42 +0900

    Update days10_Method02.java

--- days10_Method02.java
+++ days10_Method02.java
@@ -1,1 +1,13 @@
+package days10;
 
+public class Method02 
+{
+	// 매개변수 : 메서드 호출 시 메서드가 필요로하는 데이터를 전달해줄 수 있는 매개역할의 변수
+	// 매개 변수의 선언
+	// public static void 메서드이름(자료형 매개변수, 자료형 매개변수....){
+	// } 매개변수 또한 필요한 갯수만큼 선언해서 사용할 수 있습니다.
+	public static void main(String[] args)
+	{
+		
+	}
+}
