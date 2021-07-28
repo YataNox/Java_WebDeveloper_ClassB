461330f9cf509df30c342375124f1c53d31b8db6
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-28 17:29:40 +0900

    Update days10_Array15.java

--- days10_Array15.java
+++ days10_Array15.java
@@ -17,5 +17,16 @@ public class Array15
 		arr[0] = new int [3]; // arr[0][0] arr[0][1] arr[0][1]
 		arr[1] = new int [5]; // arr[1][0] arr[1][1]... arr[1][4]
 		arr[2] = new int [2]; // arr[2][0] arr[2][1] 
+		
+		 for(int i = 0; i < arr.length; i++) // 배열의 요소 값 저장
+			for(int j = 0; j < arr[i].length; j++)
+				arr[i][j] = (i+1) * (j+1);
+		 
+		 for(int[] arrAddr : arr)
+		 {
+				for(int j : arrAddr)
+					System.out.print(j + " ");
+				System.out.println();
+		 }
 	}
 }
