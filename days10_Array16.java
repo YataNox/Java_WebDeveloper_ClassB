f3def0050a7cda18e1c0a490f43f459c57d10332
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-28 17:32:41 +0900

    Update days10_Array16.java

--- days10_Array16.java
+++ days10_Array16.java
@@ -7,7 +7,17 @@ public class Array16
 		// 1차원 배열의 복사
 		int[] a = {1,2,3,4,5};
 		int[] b;
-		b = a;
+		b = a; // b배열은 a의 주소를 복사
+		
+		for(int k : a)
+			System.out.printf("%d ", k);
+		System.out.println();
+		
+		for(int k : b)
+			System.out.printf("%d ", k);
+		System.out.println();
+		
+		a[2] = 100;
 		for(int k : a)
 			System.out.printf("%d ", k);
 		System.out.println();
