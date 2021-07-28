5c217ea93c852d48c6f7b5bcc9ad85e0b1b0a738
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-28 20:44:57 +0900

    Update days10_Method04.java

--- days10_Method04.java
+++ days10_Method04.java
@@ -13,12 +13,18 @@ public class Method04
 			System.out.printf("a[%d] = %d\t", i, a[i]);
 		
 		updateValue(a);
+		// 메소드의 매개변수로 참조 변수의 값을 넘겨주는 방식(배열 이름 변수,
+		// String 참조 변수, Class 참조 변수 등)
+		// 호출된 메소드에서 참조 값을 사용해 값을 변경하게 되면 호출한 곳이
+		// 가리키는 곳의 메모리에 직접 반영됩니다.
 		
 		System.out.println("\nmain에서 출력 : ");
 		for(int i = 0; i < a.length; i++)
 			System.out.printf("a[%d] = %d\t", i, a[i]);
 	}
 	
+	// int [] b -> 전달인수로 주소(참조값)가 전달되므로 매개변수도 주소를 저장할
+	// 참조 변수가 위치하고 있어야 정상 실행됩니다.
 	public static void updateValue(int[] a)
 	{
 		a[0] = 100; a[1] = 200; a[2] = 300;
@@ -26,4 +32,7 @@ public class Method04
 		for(int i = 0; i < a.length; i++)
 			System.out.printf("a[%d] = %d\t", i, a[i]);
 	}
+	// updateValue(a[0],a[1].....);
+	// public static void updateValue(int a, int b, ....)
+	
 }
