cf6c6a42ca6207a71fe7f6980c493d31aea8acc3
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-29 18:24:15 +0900

    Update days11_Method12.java

--- days11_Method12.java
+++ days11_Method12.java
@@ -11,7 +11,10 @@ public class Method12
 		// 전달인수(매개변수)의 자료형, 순서 등이 서로 달라도 가능
 		System.out.println("두 개의 정수 중 큰 값 : " + max(11, 33));
 		System.out.println("두 개의 실수 중 큰 값 : " + max(12.8, 12.5));
-		
+	
+		// 정의된 메소드들의 매개변수들과 맞지 않은 호출은 에러입니다.
+		//System.out.println("두 개의 정수 중 큰 값 : " + max(11, 33, 55)); // 에러
+		//System.out.println("두 개의 실수 중 큰 값 : " + max(11.0, 33, 33.5)); // 에러
 	}
 	
 	public static int max(int a, int b)
