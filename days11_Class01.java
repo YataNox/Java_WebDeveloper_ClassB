4ce001100f4f18812d32c1484a8c6c3f58605bfd
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-29 20:49:17 +0900

    Update days11_Class01.java

--- days11_Class01.java
+++ days11_Class01.java
@@ -1,4 +1,28 @@
 package days11;
+// 클래스(class)
+// -> 프로그램으로 처리하고자 하는 대상을 자료화하여
+// 하나의 자료형으로 정의(속성/기능)한 사용자 정의 자료형
+// 쉽게 말해 개발자가 필요에 의해 정의한 자료형입니다.
+
+class 클래스이름
+{
+	// 멤버 필드
+	int 변수1;
+	double 변수2;
+	String 변수3;
+	int[] 배열1 = new int[5];
+	// 멤버 메소드
+	public void 메소드1(){}
+	
+	public void 메소드2()	{}
+}
+
+// 클래스 First 생성
+class First
+{
+	int n1;
+	int n2;
+}
 
 public class Class01 
 {
