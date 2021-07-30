e28698c713a89745f759b6a349f97456aa125316
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-30 19:40:46 +0900

    Create days12_Class05.java

---
+++ days12_Class05.java
@@ -0,0 +1,14 @@
+package days12;
+
+class Account
+{
+	double balance; // 계좌정보 클래스에 잔액이 저장된 멤버 변수
+}
+public class Class05 
+{
+	public static void main(String[] args) 
+	{
+		Account a = new Account();
+		a.balance = 50000;
+	}
+}
