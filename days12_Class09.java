833c49a17319ae5b87850f2923370c3df2cb86b5
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-30 21:07:57 +0900

    Update days12_Class09.java

--- days12_Class09.java
+++ days12_Class09.java
@@ -2,6 +2,15 @@ package days12;
 
 class Bclass
 {
+	private int age;
+	// 앞선 예제 Class08과 같이 클래스 내부에 생성자를 꺼내어 따로 정의하지 않으면,
+	// 컴파일러는 자동으로 클래스 내부에 숨어있는 디폴트 생성자를 호출합니다.
+	// 디폴트 생성자 : Bclass(){}와 같이 생겨서 기본 형태만 설정된 생성자
+	// 아래와 같이 내용없이 생성자를 따로 정의하는 것은, 따로 생성자를 기술하지 않는 것과  
+	// 같습니다.
+	Bclass(){}
+	public int getAge() {return age;}
+	public void setAge(int age) {this.age = age;}
 	
 }
 
@@ -9,6 +18,8 @@ public class Class09
 {
 	public static void main(String[] args)
 	{
-		
+		Bclass b = new Bclass();
+		// 따로 정의된 생성자가 없으면, 컴파일러가 클래스 내부에 숨어있는 디폴트
+		// 생성자를 호출
 	}
 }
