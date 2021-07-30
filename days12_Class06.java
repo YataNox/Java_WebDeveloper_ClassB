e15b6375806a02422141f3889ece6493bc05bba7
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-30 20:16:34 +0900

    Update days12_Class06.java

--- days12_Class06.java
+++ days12_Class06.java
@@ -1,5 +1,7 @@
 package days12;
 
+import java.util.Scanner;
+
 //접근 지정자
 // 모든 클래스의 멤버는 현실에 존재하는 대상의 정보로서,
 // 직접적인 접근이 허용되는 것과 호용되면 안되는 정보가 있습니다.
@@ -17,11 +19,72 @@ class AccountWithPermission
 	// 일반저긍로 이러한 정보들은 직접적으로 접근 및 수정해서는 안됩니다.
 	// 이러한 이유로 모든 멤버필드들은 private으로 선언하고, 멤버필드들을 제어할
 	// 수 있는 메서드들을 public으로 선언합니다.
+	private double balance;
+	
+	public void initBalance(int money) {balance = money;}
+	
+	public void withraw(int money)
+	{
+		if(money > balance) 
+		{
+			System.out.println("잔액이 부족합니다.");
+		// void 메서드의 return은 반환 될 값 없이 사용되며, 메소드를 현 위치에서
+		// 종료하는 명령입니다.
+			return;
+		}
+		balance -= money;
+	}
+	
+	public void deposit(int money)
+	{
+		if(money <= 0) 
+		{
+			System.out.println("입금액 오류. 관리자에게 문의하세요.");
+			return;
+		}
+		balance += money;
+	}
+	
+	public void display()
+	{
+		System.out.printf("현재 잔액은 %.1f원 입니다.\n", balance);
+	}
 }
+
 public class Class06 
 {
 	public static void main(String[] args) 
 	{
+		Scanner sc = new Scanner(System.in);
+		int money, selectMenu;
+		
+		AccountWithPermission a = new AccountWithPermission(); // 객체 생성
+		a.initBalance(50000); // 잔액 초기화
+		
+		System.out.printf("메뉴 선택 : 1. 입금	2. 출금	3. 잔액확인	4. 종료 : ");
+		selectMenu = sc.nextInt();
+		
+		while(selectMenu != 4) 
+		{
+			switch(selectMenu)
+			{
+				case 1:
+					System.out.printf("입금할 금액을 입력하세요 : ");
+					money = sc.nextInt();
+					a.deposit(money);
+					break;
+				case 2:
+					System.out.printf("출금할 금액을 입력하세요 : ");
+					money = sc.nextInt();
+					a.withraw(money);
+					break;
+				case 3:
+					a.display();
+					break;
+			}
+			System.out.printf("메뉴 선택 : 1. 입금	2. 출금	3. 잔액확인	4. 종료 : ");
+			selectMenu = sc.nextInt();
+		}
 		
 	}
 }
