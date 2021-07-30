3d82ffcb30e3f4ad4aa4754aee380cbf5594935a
YataNox <65604233+YataNox@users.noreply.github.com>
2021-07-30 20:43:07 +0900

    Update days12_Class07.java

--- days12_Class07.java
+++ days12_Class07.java
@@ -14,9 +14,19 @@ class GetSetTest
 	// 멤버 변수가 하나 만들어지면, 그 변수에 값을 세팅할 수 잇는 메소드와 값을 얻어
 	// 낼 수 있는 메소드를 생성합니다.
 	// 멤버 변수의 값을 저장하는 메서드 이름 : setNum
-	public void setNum(int n) {num = n;}
 	// 멤버 변수의 값을 리턴해주는 메서드 이름 : getNum
-	public int getNum() {return num;}
+	//	public void setNum(int n) {num = n;}
+	//	public int getNum() {return num;}
+	//	
+	//	public void setDou(double d) {dou = d;}
+	//	public double getDou() {return dou;}
+	//
+	//	public void setStr(String s) {str = s;}
+	//	public String getStr() {return str;}
+	
+	// getter와 setter의 자동 생성
+	// 현 위치에서 마우스 오른쪽 버튼 클릭
+	// -> Source 메뉴 -> Generate Getters and Setters 선택
 }
 
 public class Class07 
@@ -24,8 +34,8 @@ public class Class07
 	public static void main(String[] args) 
 	{
 		GetSetTest gs = new GetSetTest();
-		gs.setNum(12345);
-		int n = gs.getNum();
-		System.out.println("멤버 변수의 num값은 " + n + "입니다.");
+		//	gs.setNum(12345);
+		//	int n = gs.getNum();
+		// System.out.println("멤버 변수의 num값은 " + n + "입니다.");
 	}
 }
