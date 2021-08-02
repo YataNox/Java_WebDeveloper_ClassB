af545bcedf9906933fd2ece0787af7f182771fa6
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-02 21:04:53 +0900

    Update days13_Class22.java

--- days13_Class22.java
+++ days13_Class22.java
@@ -14,7 +14,10 @@ class MemberCall
 	// 인스턴스 변수 : 객첵 생성될 때 iv2가 생성 -> 이미 만들어져서 20을 저장하고 잇는
 	// cv 값으로 초기화 -> 가능
 	int iv2 = sv;
-	static int sv2 = iv; // 에러 : iv가 언제 생성될지 모르므로, sv2에 값을 전달할 수 없다.
+	// static int sv2 = iv; 
+	// 에러 : iv가 언제 생성될지 모르므로, sv2에 값을 전달할 수 없다.
+	
+	static int sv2 = new MemberCall().iv;
 }
 
 public class Class22 
