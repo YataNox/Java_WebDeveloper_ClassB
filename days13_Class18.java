9a6fae5f31a25a5889535830dee11b9ef85f340f
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-02 18:39:22 +0900

    Update days13_Class18.java

--- days13_Class18.java
+++ days13_Class18.java
@@ -1,9 +1,23 @@
 package days13;
 
+class StaticB
+{
+	static int count = 1;
+	int num;
+	public StaticB()
+	{
+		this.num = count++;
+		// 생성자 메소드에서 count 값을 1씩 증가시키며, 그 값을 num에 저장
+	}
+}
+
 public class Class18 
 {
 	public static void main(String[] args)
 	{
-		
+		StaticB b1 = new StaticB();
+		System.out.printf("num = %d, count = %d\n", b1.num, StaticB.count);
+		StaticB b2 = new StaticB();
+		System.out.printf("num = %d, count = %d\n", b2.num, StaticB.count);
 	}
 }
