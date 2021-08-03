8914ec8c96332aa62ec3e1ddc97c68149dcd05b7
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-03 20:12:25 +0900

    Create Extends02.java

---
+++ days14/Extends02.java
@@ -0,0 +1,15 @@
+package days14;
+
+// 상속의 구현
+// extends 키워드의 사용
+// class SubClass명(자식 클래스) extends SuperClass명(부모 클래스)
+// 상속을 받는 클래스 : 서브 클래스, 자식 클래스, 하위 클래스
+// 상속을 제공 해주는 클래스 : 슈퍼 클래스, 부모 클래스, 상위 클래스
+
+public class Extends02 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
