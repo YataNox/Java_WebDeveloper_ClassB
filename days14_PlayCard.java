47783faab3272f87c986fac4ffc4d7fefa06c3ab
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-03 18:39:07 +0900

    Update days14_PlayCard.java

--- days14_PlayCard.java
+++ days14_PlayCard.java
@@ -1,5 +1,7 @@
 package days14;
 
+import java.util.Random;
+
 class CardDeck
 {
 	Card[] cards = new Card[52];
@@ -12,6 +14,20 @@ class CardDeck
 			for(int j = 1; j <= 13; j++) // 카드 번호 1~13 반복 
 				cards[count++] = new Card(i, j); // 카드장 수 i는 0~51
 	}
+	
+	public void shuffle()
+	{
+		Random rd = new Random();
+		for(int i = 0; i < 52; i++)
+		{
+			// int r = (int)(Math.random()  * 52);
+			int r = Math.abs(rd.nextInt() % 52);
+			Card temp = cards[i];
+			cards[i] = cards[r];
+			cards[r] = temp;
+		}
+		
+	}
 }
 
 public class PlayCard 
@@ -19,7 +35,8 @@ public class PlayCard
 	public static void main(String[] args)
 	{
 		CardDeck d = new CardDeck();
-		System.out.println(d.cards[0].toString());
+		d.shuffle();
+		System.out.println(d.cards[12].toString());
 		System.out.println(d.cards[51]);
 	}
 }
