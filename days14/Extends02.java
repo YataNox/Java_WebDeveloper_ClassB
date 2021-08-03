25c7a2d2997d8ce194f6c8d1d0438afd608c2dee
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-03 20:16:48 +0900

    Update Extends02.java

--- days14/Extends02.java
+++ days14/Extends02.java
@@ -6,10 +6,29 @@ package days14;
 // 상속을 받는 클래스 : 서브 클래스, 자식 클래스, 하위 클래스
 // 상속을 제공 해주는 클래스 : 슈퍼 클래스, 부모 클래스, 상위 클래스
 
+class Car
+{
+	int 엔진;
+	int 배기량;
+	int 도어개수;
+}
+
+class K7 extends Car // 자식 클래스 선언
+{
+	public void printInfo() // 자식 클래스에서는 상속받은 멤버를 사용할 수 있음
+	{
+		System.out.printf("엔진 : %d\n", this.엔진);
+		System.out.printf("배기량 : %d\n", this.배기량);
+		System.out.printf("도어개수 : %d\n", this.도어개수);
+	}
+}
+
 public class Extends02 
 {
 	public static void main(String[] args)
 	{
-		
+		K7 k = new K7();
+		k.엔진 = 6; k.배기량 = 3000; k.도어개수 = 5;
+		k.printInfo();
 	}
 }
