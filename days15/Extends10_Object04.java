8b53261c9cafe8e087a3475012efedd5c0944056
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-04 19:48:30 +0900

    Update Extends10_Object04.java

--- days15/Extends10_Object04.java
+++ days15/Extends10_Object04.java
@@ -27,5 +27,12 @@ public class Extends10_Object04
 		System.out.println("학생 1번의 정보 : " + s1);
 		System.out.println("학생 2번의 정보 : " + s2);
 		System.out.println("학생 3번의 정보 : " + s3);
+		
+		// 레퍼런스 변수들간의 비교
+		if(s1 == s2)
+			System.out.println("s1 변수와 s2 변수는 같습니다.(s1 == s2)");
+		else
+			System.out.println("s1 변수와 s2 변수는 다릅니다.(s1 == s2)");
+		// 결과 : s1 변수와 s2 변수는 다릅니다.(s1 == s2)
 	}
 }
