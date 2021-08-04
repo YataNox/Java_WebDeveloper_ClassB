e44d114ef090b59d7b57780ce8a2e4a1286fc356
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-04 20:09:59 +0900

    Update Extends10_Object04.java

--- days15/Extends10_Object04.java
+++ days15/Extends10_Object04.java
@@ -15,6 +15,27 @@ class Student
 		info += "age = " + this.age;
 		return info;
 	}
+	// s1.equals(s2) -> this <- s1 obj <- s2
+	public boolean equals(Object obj)
+	{
+		if(!(obj instanceof Student))
+			return false;
+		
+		Student s = (Student)obj;
+		// 부모 클래스의 레퍼런스는 자식 인스턴스를 가졌을 때, 부모가 물려준 멤버는 접근이
+		// 가능하지만, 자식이 새로 만든 멤버는 접근이 불가능합니다.
+		// 따라서 this와 obj가 비교가 되려면, obj를 Student 형으로 변환해야합니다.
+		// Student s = (Student)obj;
+		// 또한 전달된 obj가 현재로선 어떤 인스턴스를 갖고 있는지 모르기때문에,
+		// instanceof를 이용하여 형변환이 가능한지 보고 변환합니다.
+		// 이상한 클래스가 왔으면 그냥 바로 return(메서드 종료)
+		boolean flag_name = this.name.equals(s.name);
+		boolean flag_age = this.age == s.age;
+		if(flag_name && flag_age)
+			return true;
+		else
+			return false;
+	}
 }
 
 public class Extends10_Object04 
@@ -27,12 +48,28 @@ public class Extends10_Object04
 		System.out.println("학생 1번의 정보 : " + s1);
 		System.out.println("학생 2번의 정보 : " + s2);
 		System.out.println("학생 3번의 정보 : " + s3);
-		
+		s1.equals(s3);
 		// 레퍼런스 변수들간의 비교
 		if(s1 == s2)
 			System.out.println("s1 변수와 s2 변수는 같습니다.(s1 == s2)");
 		else
 			System.out.println("s1 변수와 s2 변수는 다릅니다.(s1 == s2)");
 		// 결과 : s1 변수와 s2 변수는 다릅니다.(s1 == s2)
+		
+		if(s1.equals(s2))
+		System.out.println("s1 변수와 s2 변수는 같습니다.(s1.equals(s2))");
+	else
+		System.out.println("s1 변수와 s2 변수는 다릅니다.(s1.equals(s2))");
+		
+		// .equals 메서드를 사용하여 비교(equals 메서드 오버라이드 이후)
+		if(s1.equals(s2))
+			System.out.println("s1 변수와 s2 변수는 같습니다.(s1.equals(s2))");
+		else
+			System.out.println("s1 변수와 s2 변수는 다릅니다.(s1.equals(s2))");
+		
+		if(s1.equals(s3))
+			System.out.println("s1 변수와 s3 변수는 같습니다.(s1.equals(s3))");
+		else
+			System.out.println("s1 변수와 s3 변수는 다릅니다.(s1.equals(s3))");
 	}
 }
