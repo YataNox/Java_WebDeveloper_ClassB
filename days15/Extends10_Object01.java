b7ceef6056b62ceb4ca3b5037a86f5697654443f
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-04 18:16:46 +0900

    Update Extends10_Object01.java

--- days15/Extends10_Object01.java
+++ days15/Extends10_Object01.java
@@ -7,11 +7,24 @@ package days15;
 // Subclass extends Super 라는 건 Subclass는 Object 클래스를 상속하지 않지만
 // Super 클래스가 Object 클래스를 상속하므로, Subclass는 Object 클래스를
 // 상위의 상위로 상속한 것과 같은 의미입니다.
+class UserClass // extends Object
+{
+	
+}
 
 public class Extends10_Object01 
 {
 	public static void main(String[] args)
 	{
-		
+		UserClass obj = new UserClass();
+		// getClass 메소드는 해당 객체의 클래스 이름을 리턴해주는 메서드
+		System.out.println(obj.getClass());
+		// hashCode 메소드는 해당 객체의 해시 코드 값(다른 객체와 구분하기 위한 고유 값)을
+		// 리턴해주는 메소드(JVM에 의해서 관리되고 있는 번호
+		System.out.println(obj.hashCode());
+		// toString 메소드는 해당 객체의 정보를 문자열로 리턴해주는 메소드
+		System.out.println(obj.toString());
+		// 객체의 클래스명 + '@' + 해시코드값(16진수)
+		System.out.println(obj); // System.out.println(obj.toString()); 와 같음
 	}
 }
