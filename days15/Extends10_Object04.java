d8a8a5ea187ddb087329e79a4de03c576c76b438
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-04 19:46:13 +0900

    Update Extends10_Object04.java

--- days15/Extends10_Object04.java
+++ days15/Extends10_Object04.java
@@ -1,9 +1,31 @@
 package days15;
 
+class Student
+{
+	private String name;
+	private int age;
+	public Student(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	public String toString()
+	{
+		String info = "name = " + this.name + ", ";
+		info += "age = " + this.age;
+		return info;
+	}
+}
+
 public class Extends10_Object04 
 {
 	public static void main(String[] args)
 	{
-		
+		Student s1 = new Student("홍길동", 21);
+		Student s2 = new Student("홍길동", 21);
+		Student s3 = new Student("홍길서", 22);
+		System.out.println("학생 1번의 정보 : " + s1);
+		System.out.println("학생 2번의 정보 : " + s2);
+		System.out.println("학생 3번의 정보 : " + s3);
 	}
 }
