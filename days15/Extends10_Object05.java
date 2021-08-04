c71a0c8a39f03480ea4434f16ab9ed565f9be7bd
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-04 20:13:11 +0900

    Create Extends10_Object05.java

---
+++ days15/Extends10_Object05.java
@@ -0,0 +1,20 @@
+package days15;
+
+class Product
+{
+	int price;
+	int bonusPoint;
+	Product(int p)
+	{
+		this.price = p;
+		this.bonusPoint = p / 10;
+	}
+}
+
+public class Extends10_Object05 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
