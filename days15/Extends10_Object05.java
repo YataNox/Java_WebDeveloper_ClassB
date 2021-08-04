d79eda8490cb4e0d53bc72ee3803b4faf7b08062
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-04 20:19:38 +0900

    Update Extends10_Object05.java

--- days15/Extends10_Object05.java
+++ days15/Extends10_Object05.java
@@ -11,10 +11,40 @@ class Product
 	}
 }
 
+class Computer extends Product
+{
+	Computer()
+	{
+		super(150);
+	}
+	public String toString() {return "Computer";}
+}
+
+class Tv extends Product
+{
+	Tv()
+	{
+		super(100);
+	}
+	public String toString() {return "Tv";}
+}
+
+class Audio extends Product
+{
+	Audio()
+	{
+		super(60);
+	}
+	public String toString() {return "Audio";}
+}
+
 public class Extends10_Object05 
 {
 	public static void main(String[] args)
 	{
-		
+		Tv t1 = new Tv();
+		Tv t2 = new Tv();
+		Computer c = new Computer();
+		Audio a = new Audio();
 	}
 }
