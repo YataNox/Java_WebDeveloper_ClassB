d33d778a26307ab1cd2a5ccb404f68653384d54b
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 17:23:43 +0900

    Create Extends12_Interface02.java

---
+++ days16/Extends12_Interface02.java
@@ -0,0 +1,17 @@
+package days16;
+
+// 인터페이스는 멤버 필드로 public static final 멤버만 가질 수 있습니다.
+// 인터페이스는 멤버 메소드로 public abstract 메소드만 가질 수 있습니다.
+interface InterB
+{
+	public static final int n1 = 1;
+	public static final int n2 = 2;
+}
+
+public class Extends12_Interface02 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
