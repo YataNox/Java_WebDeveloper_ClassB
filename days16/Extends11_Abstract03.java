feaf984d47c327905b02992cc1a1fa4a816ffd37
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 16:45:31 +0900

    Update Extends11_Abstract03.java

--- days16/Extends11_Abstract03.java
+++ days16/Extends11_Abstract03.java
@@ -6,9 +6,27 @@ package days16;
 // 자식 클래스에서 사용하지 않을 추상 메소드라도, 객체 생성을 위해 반드시 구현해야
 // 하는 문제점이 있습니다.
 
-abstract class AbsractA
+abstract class AbstractA
 {
-	
+	public abstract void test1();
+	public abstract void test2();
+	public abstract void test3();
+	public abstract void test4();
+	public abstract void test5();
+	public abstract void test6();
+	public abstract void test7();
+}
+// Abstract_A 클래스를 상속 받은 클래스는 필요없는 메소드가 있어도, 추상 메소드를 모두
+// 오버라이드(구현)해야합니다.
+class AbstractA_Sub1 extends AbstractA
+{
+	public void test1() {}
+	public void test2() {}
+	public void test3() {}
+	public void test4() {}
+	public void test5() {}
+	public void test6() {}
+	public void test7() {}
 }
 
 public class Extends11_Abstract03 
