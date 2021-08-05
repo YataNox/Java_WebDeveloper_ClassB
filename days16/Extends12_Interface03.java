3d79ad0af7d937863433d986411faec716b2dc76
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 17:30:36 +0900

    Create Extends12_Interface03.java

---
+++ days16/Extends12_Interface03.java
@@ -0,0 +1,18 @@
+package days16;
+
+interface InterC_Super1
+{
+	public abstract void interC1Test();
+}
+interface InterC_Super2
+{
+	public abstract void interC2Test();
+}
+
+public class Extends12_Interface03 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
