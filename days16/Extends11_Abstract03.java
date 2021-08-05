97839b7f38ffdbcabaf497592b92ee36cae9acb9
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 16:40:56 +0900

    Create Extends11_Abstract03.java

---
+++ days16/Extends11_Abstract03.java
@@ -0,0 +1,20 @@
+package days16;
+
+// 추상 클래스의 단점
+// 추상 메소드 구현의 강제성!!
+// 추상 메소드의 갯수가 많으면 상속에 부담을 주게 됩니다.
+// 자식 클래스에서 사용하지 않을 추상 메소드라도, 객체 생성을 위해 반드시 구현해야
+// 하는 문제점이 있습니다.
+
+abstract class AbsractA
+{
+	
+}
+
+public class Extends11_Abstract03 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
