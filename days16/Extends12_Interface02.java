26100192fb5f9a32c49667139ac2137ceb15e5e1
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 17:27:55 +0900

    Update Extends12_Interface02.java

--- days16/Extends12_Interface02.java
+++ days16/Extends12_Interface02.java
@@ -6,6 +6,17 @@ interface InterB
 {
 	public static final int n1 = 1;
 	public static final int n2 = 2;
+	// 인터페이스 내부에 선언되는 멤버 필드는 public static final을 생략할 수 있습니다.
+	int n3 = 3;
+	// public static final이 생략된 변수와 일반 멤버 변수의 구분은 초기 값의 유무에
+	// 따라 구분 됩니다.
+	// int n3; // 값이 초기화 되지 않는 변수는 일반 멤버 변수로 인식되어 에러
+	public abstract void test1();
+	public abstract void test2();
+	// 인터페이스 내부에 선언되는 메소드는 public abstract을 생략할 수 있습니다.
+	void test3();
+	// public abstract가 생략된 메서드와 혀용되지 않는 일반 메소드의 구분은 {}의
+	// 유무에 따라 구분합니다.
 }
 
 public class Extends12_Interface02 
