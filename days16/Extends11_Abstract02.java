cbf99c82d4eb3d0398c331d647cc43b80f3e49d3
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 16:38:04 +0900

    Update Extends11_Abstract02.java

--- days16/Extends11_Abstract02.java
+++ days16/Extends11_Abstract02.java
@@ -17,6 +17,7 @@ abstract class AbstractAnimal
 // 단, 추상 메소드를 포함할 수 있는 특징이 추가된 클래스입니다.
 // 추상 클래스는 상속을 통한 다형성 구현을 위해서 생성됩니다.
 
+// 추상 클래스를 상속한 자식 클래스가 추상 메소드를 오버라이딩하지 않는다면 에러
 class DogA extends AbstractAnimal
 {
 	public void sound()
@@ -33,10 +34,23 @@ class CatA extends AbstractAnimal
 	}
 }
 
+//추상 메소드를 오버라이딩한 자식 클래스는 일반 클래스로 사용될 수 있습니다.
+//추상 클래스로 추상 클래스만의 객체를 만들수는 없지만
+//추상 클래스(부모)의 레퍼런스 변수로, 자식 클래스의 인스턴스 주소는 저장할 수 있습니다.
+// AbstractAnimal a = new AbstractAnimal(); x
+// 추상 클래스는 내부에 완전치 못한 추상 메소드가 있으므로 객체 생성이 안되지만
+// AbstractAnimal a = new DogA(); o
+// AbstractAnimal a = new CatA(); o
+// 자식들의 인스턴스를 저장할 레퍼런스 변수로는 사용이 가능합니다.(다형성의 구현)
+
 public class Extends11_Abstract02 
 {
 	public static void main(String[] args)
 	{
+		AbstractAnimal dog = new DogA();
+		AbstractAnimal cat = new CatA();
 		
+		dog.sound();
+		cat.sound();
 	}
 }
