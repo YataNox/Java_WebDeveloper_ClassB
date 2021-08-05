a8568ad833eb2fe0fdecfc57debdf37d87ee8c76
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 16:27:37 +0900

    Create Extends11_Abstract02.java

---
+++ days16/Extends11_Abstract02.java
@@ -0,0 +1,20 @@
+package days16;
+
+// 추상 클래스 (Abstract Class)
+// 상속을 통한 다형성의 구현(메소드 오버라이딩)에 강제성을 부여하기 위한 클래스
+// 추상 메소드를 포함한 클래스 : 추상 메소드를 하나 이상 포함하면 추상 클래스라고 부릅니다.
+// 추상 메소드 : 메소드의 원형만 존재하고 실제 내용이 없는 메소드
+
+abstract class AbstractAnimal
+{
+	public abstract void sound();
+	// 메소드 정의만 있고, 메소드의 body(몸체)구현은 없습니다.
+}
+
+public class Extends11_Abstract02 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
