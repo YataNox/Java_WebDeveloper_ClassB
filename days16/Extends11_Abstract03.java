ed0d32b2df5ec34908a9a59e4adc51c377ce46fc
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-05 16:48:12 +0900

    Update Extends11_Abstract03.java

--- days16/Extends11_Abstract03.java
+++ days16/Extends11_Abstract03.java
@@ -29,6 +29,28 @@ class AbstractA_Sub1 extends AbstractA
 	public void test7() {}
 }
 
+class AbstractA_Adapter extends AbstractA
+{
+	public void test1() {}
+	public void test2() {}
+	public void test3() {}
+	public void test4() {}
+	public void test5() {}
+	public void test6() {}
+	public void test7() {}
+}
+
+// 모든 추상 클래스가 구현(오버라이드)된 AbstracA_Adapter 클래스를 상속받아
+// 사용하면 필요없는 메서드를 강제로 구현(오버라이드)하지 않고 필요한 것만 구현하여
+// 사용할 수 있습니다.
+class AbstractA_Sub2 extends AbstractA_Adapter
+{
+	public void test1() 
+	{
+		System.out.println("test1 오버라이딩~!");
+	}
+}
+
 public class Extends11_Abstract03 
 {
 	public static void main(String[] args)
