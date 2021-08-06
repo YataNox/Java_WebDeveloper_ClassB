455fcab08d3017b788a62207e216db147fbf6dc0
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-06 17:16:35 +0900

    Update Calendar04.java

--- days17/Calendar04.java
+++ days17/Calendar04.java
@@ -21,5 +21,15 @@ public class Calendar04
 		System.out.println(toDate(date));
 		// add 메소드의 증감은 하나의 요소를 변경하지만, 그로인해 다른 필드에 영향을 줄 수 있습니다.
 		// 일을 변경하여 달이 바뀌거나, 월을 변경하여 연도가 바뀌는 변경 등
+		System.out.println("= 6달 후 =");
+		date.add(Calendar.MONTH, 6);
+		System.out.println(toDate(date));
+		
+		System.out.println("= 40일 후 (roll) = ");
+		date.roll(Calendar.DATE, 40);
+		// 다른 필드에 영향을 주지 않고 해당 필드만으로 더하고 빼는 메소드
+		// 3월 1일에서 30일 더하면 3월 31일(30일)이 되고, 이 후 10일이 더해져 다시 10이 나옴
+		// 월에는 영향을 주지 않음 결과 3월 10일
+		System.out.println(toDate(date));
 	}
 }
