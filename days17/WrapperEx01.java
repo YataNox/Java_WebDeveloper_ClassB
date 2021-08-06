5ffae08a25e9e6053b5fbd62539ae04f3bff2647
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-06 18:25:02 +0900

    Update WrapperEx01.java

--- days17/WrapperEx01.java
+++ days17/WrapperEx01.java
@@ -24,5 +24,19 @@ public class WrapperEx01
 		System.out.println("i1.compareTo(i2) = " + i1.compareTo(i2));
 		// 실제 인스턴스에 저장된 값들의 비교 - 뺄셈 연산으로 비교 앞의 숫자가 크면 양수 뒤에 숫자가 크면 음수 반환
 		// 같으면 0 반환
+		
+		// Object 부모 클래스의 메소드 오버라이딩
+		System.out.println("i1.toString() = " + i1.toString());
+		
+		// 표현 가능한 가장 큰 값(Static 변수)
+		System.out.println("MAX_VALUE = " + Integer.MAX_VALUE);
+		// 표현 가능한 가장 작은 값(Static 변수)
+		System.out.println("MIN_VALUE = " + Integer.MIN_VALUE);
+		
+		// 비트 바이트로 표현한 저장 크기
+		System.out.println("SIZE = " + Integer.SIZE + " bits");
+		System.out.println("BYTES = " + Integer.BYTES + " bytes");
+		// 자료형
+		System.out.println("TYPE = " + Integer.TYPE);
 	}
 }
