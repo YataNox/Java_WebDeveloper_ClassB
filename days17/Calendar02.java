2df6451e3cd6076eb00af598ace52f797a11b927
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-06 16:28:59 +0900

    Update Calendar02.java

--- days17/Calendar02.java
+++ days17/Calendar02.java
@@ -1,11 +1,30 @@
 package days17;
 
+import java.util.Calendar;
+
 public class Calendar02 
 {
+	// Calendar2 클래스 내부에서 사용할 멤버 변수(배열).
+	// static이므로 언제든 사용 가능하고, 같은 클래스 내부에서 Calendar2.을 앞에 붙이지 않고
+	// 사용이 가능합니다.
 	static String[] weekday = {"", "일", "월", "화", "수", "목", "금", "토"};
 	
 	public static void main(String[] args)
 	{
+		Calendar date1 = Calendar.getInstance();
+		Calendar date2 = Calendar.getInstance();
 		
+		// date1 달력 객체의 오늘 날짜를 2015년 8월 15일로 설정
+		date1.set(2015, 7, 15); //원하는 특정 날짜로 객체 내 날짜 구성을 재설정, date2는 아직 오늘 날짜
+		// set 메소드에 필드명과 값을 넣어주면 해당 필드 값만 변경됩니다.
+		// date1.set(Calendar.MONTH, 6) -> 월만 7월로 바뀜
+		
+		// 위 date1에 설정된 날짜를 0000년 0월 0일 형식으로 출력해주세요
+		// system.out.println()안에 한번에 구성해서 출력해주세요.
+		System.out.println("date1 : " + date1.get(Calendar.YEAR) + "년 " + (date1.get(Calendar.MONTH) + 1) + "월"
+				+ date1.get(Calendar.DATE) + "일");
+		System.out.println("date2 : " + date2.get(Calendar.YEAR) + "년 " + (date2.get(Calendar.MONTH) + 1) + "월"
+				+ date2.get(Calendar.DATE) + "일");
 	}
+	
 }
