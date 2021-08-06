d879da32c3e2a83b0b02dae4d632ead2d21c902a
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-06 21:04:12 +0900

    Update Formatter04.java

--- days17/Formatter04.java
+++ days17/Formatter04.java
@@ -1,9 +1,14 @@
 package days17;
 
+import java.text.MessageFormat;
+
 public class Formatter04 
 {
 	public static void main(String[] args)
 	{
-		
+		String msg = "Name : {0}		Tel : {1}	Age : {2}	Birthday : {3}";
+		Object[] value = {"이자바", "02-123-1234", 27, "07-09"};
+		String r = MessageFormat.format(msg, value); // 메세지 포맷의 메소드에 의한 배열 병합 작업
+		System.out.println(r);
 	}
 }
