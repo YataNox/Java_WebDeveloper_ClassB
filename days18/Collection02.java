11f0e898820d8500a5c37e3ddfc9aa2a778d02f2
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-09 16:33:08 +0900

    Create Collection02.java

---
+++ days18/Collection02.java
@@ -0,0 +1,16 @@
+package days18;
+
+// 컬렉션 클래스 저장 방식
+// 모든 컬렉션 클래스들은 기본적으로 Object 타입을 저장/반환할 수 있습니다.
+
+// Object 타입을 매개변수 사용하는 모든 컬렉션 클래스들은 타입에 상관없이 저장할 수 있음
+// 하지만, 저장된 데이터를 반환받는 과정에서 런타임에러가 발생할 수 있습니다.
+// Object -> 자식 클래스의 타입 : 강제형 변환
+
+public class Collection02 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
