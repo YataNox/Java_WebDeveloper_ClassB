34b929efbb86038640bd489c91e97decffb2fa6f
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-09 16:38:28 +0900

    Update Collection02.java

--- days18/Collection02.java
+++ days18/Collection02.java
@@ -1,5 +1,7 @@
 package days18;
 
+import java.util.ArrayList;
+
 // 컬렉션 클래스 저장 방식
 // 모든 컬렉션 클래스들은 기본적으로 Object 타입을 저장/반환할 수 있습니다.
 
@@ -11,6 +13,24 @@ public class Collection02
 {
 	public static void main(String[] args)
 	{
+		ArrayList a = new ArrayList();
+		// 다양한 타입을 저장할 수 있는 컬렉션 클래스의 객체
+		a.add(10);
+		a.add(1.1);
+		a.add("Hello");
+		
+		Integer i0 = (Integer)a.get(0);
+		
+		// 컬렉션 내부의 데이터를 추출하는 과정에서 일치하지 않는 타입으로 형변환이 일어나는 경우
+		// 런타임 에러가 발생되어 프로그램이 강제 종료
+		// Integer i1 = (Integer)a.get(1); // 에러
+		Double i1 = (Double) a.get(1);
+		// String i2 = a.get(2); // 에러
+		String i2 = (String)a.get(2);
+		
+		System.out.printf("i0 -> %d\n", i0);
+		System.out.printf("i0 -> %.1f\n", i1);
+		System.out.printf("i0 -> %s\n", i2);
 		
 	}
 }
