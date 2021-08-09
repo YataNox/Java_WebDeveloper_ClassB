17a24f2f361967bd79f736c4aad89d807864018e
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-09 17:30:30 +0900

    Create Collection07.java

---
+++ days18/Collection07.java
@@ -0,0 +1,19 @@
+package days18;
+
+//Hashtable, HashMap 클래스
+// 데이터베이스 내부의 키 값을 검색하기 위해서 만들어진 알고리즘을 기반으로 작성된 클래스
+// - 검색을 위해서 사용되는 클래스
+// - Key, Value를 저장할 수 있는 클래스
+// - Key는 중복을 허용하지 않습니다.
+// - Value는 중복을 허용합니다.
+// 저장 예
+// HashMap hm = new HashMap();
+// hm.put(1, "One"); hm.put(2, "Two"); hm.put(3, "Three");
+
+public class Collection07 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
