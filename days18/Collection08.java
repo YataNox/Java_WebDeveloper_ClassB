3f9194cf62dd6dbd9412a9232dbcb536e30d0b62
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-09 17:53:35 +0900

    Update Collection08.java

--- days18/Collection08.java
+++ days18/Collection08.java
@@ -21,5 +21,26 @@ public class Collection08
 		hm.put(4, "Four"); hm.put(5, "Five");
 		System.out.printf("ht.size() -> %d\n", ht.size());
 		System.out.printf("hm.size() -> %d\n", hm.size());
+		
+		// 키 값은 중복이 없고 벨류 값은 중복되게 추가
+		ht.put("Six", 1); ht.put("seven", 2);
+		hm.put(6, "Six"); hm.put(7, "Seven");
+		System.out.printf("ht.size() -> %d\n", ht.size());
+		System.out.printf("hm.size() -> %d\n", hm.size());
+		
+		// 동일한 키 값으로 다른 Value가 입력되는 경우 기존의 Value 값이 수정됩니다.
+		ht.put("One", 111);
+		hm.put(1, "First");
+		for(String k : ht.keySet())
+		{
+			Integer v = ht.get(k);
+			System.out.printf("k(%s) = v(%d) ", k, v);
+		}
+		System.out.println();
+		for(Integer k : hm.keySet())
+		{
+			String v = hm.get(k);
+			System.out.printf("k(%d) = v(%s) ", k, v);
+		}
 	}
 }
