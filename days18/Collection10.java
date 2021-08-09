e2864bdd6594ee32d22b4e8d0c3daef3cbe39bb1
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-09 18:16:03 +0900

    Update Collection10.java

--- days18/Collection10.java
+++ days18/Collection10.java
@@ -1,9 +1,25 @@
 package days18;
 
+import java.util.ArrayList;
+
+class Point
+{
+	private int x;
+	private int y;
+	public Point(int x, int y) {this.x = x; this.y = y;}
+	public String toString() {return "x : " + x + ", y " + y;}
+}
+
 public class Collection10 
 {
 	public static void main(String[] args)
 	{
-		
+		ArrayList<Point> list = new ArrayList<Point>();
+		Point p1 = new Point(10,10);
+		list.add(p1);
+		list.add(new Point(20, 20));
+		list.add(new Point(30, 30));
+		for(Point p : list)
+			System.out.println(p.toString());
 	}
 }
