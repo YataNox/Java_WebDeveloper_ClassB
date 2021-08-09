e1bd41477a370782ac7ed0b8ce9ce0d5b34d4539
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-09 17:22:46 +0900

    Update Collection06.java

--- days18/Collection06.java
+++ days18/Collection06.java
@@ -25,5 +25,16 @@ public class Collection06
 			System.out.printf("%d		", iter.next());
 		// 제어권을 넘겨받아서 위와 같이 한 번 리스트를 모두 순회하면,
 		// iter = lotto.iterator();를 통해서 다시 방문 필요할 때 제어권을 다시 얻어내야합니다.
+		System.out.println();
+		
+		// 2. for문을 사용하는 방법(개선된 형태)
+		// - for(자료형 변수명 : 순회할 수 있는 타입(배열, 컬렉션))
+		// 배열/ 컬렉션으로부터 추출한 병수를 사용하는 코드
+		for(Integer i : lotto)
+			System.out.printf("%d ", i);
+		// Integer i 부분 : 오른쪽의 배열이나 리스트의 아이템 하나를 저장할 변수선언
+		// lotto 부분 : 왼쪽 변수에 차례로 하나씩 전달해 줄수 있는 아이템을 보유한 컬렉션 또는 배열
+		// 오른쪽 리스트 또는 배열의 값을 차례로 하나씩 왼쪽 변수에 값을 전달하면서 1회의 반복 실행을 진행합니다.
+		System.out.println();
 	} 
 }
