73062df4be8a736dd1b828ec066c24300e5b723c
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-10 20:34:02 +0900

    Create Swing12.java

---
+++ days19/Swing12.java
@@ -0,0 +1,19 @@
+package days19;
+
+import javax.swing.JFrame;
+
+class JPanelTest extends JFrame
+{
+	JPanelTest()
+	{
+		
+	}
+}
+
+public class Swing12 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
