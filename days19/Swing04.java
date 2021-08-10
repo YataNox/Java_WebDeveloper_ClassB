c8df00a715816c3d9f71f53d74c2a8e9e0dd1872
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-10 17:19:18 +0900

    Update Swing04.java

--- days19/Swing04.java
+++ days19/Swing04.java
@@ -1,9 +1,17 @@
 package days19;
 
+class JImageTest extends JFrame
+{
+	JImageTest()
+	{
+		
+	}
+}
+
 public class Swing04 
 {
 	public static void main(String[] args)
 	{
-		
+		new JImageTest();
 	}
 }
