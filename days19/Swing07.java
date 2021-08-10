97797f4808c4dd3b90bb2e27ace8bc9342a92449
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-10 18:04:31 +0900

    Update Swing07.java

--- days19/Swing07.java
+++ days19/Swing07.java
@@ -1,12 +1,17 @@
 package days19;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 class JBorderLayout extends JFrame
 {
 	JBorderLayout()
 	{
-		
+		Container con = getContentPane();
+		con.setLayout(new BorderLayout(10, 10));
+		// 각 지역(동서남북중앙)간의 간격 가로세로 10px
 	}
 }
 
