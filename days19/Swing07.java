773b91b606b38cc8c51eb85b6cbb147c36715289
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-10 18:03:28 +0900

    Create Swing07.java

---
+++ days19/Swing07.java
@@ -0,0 +1,19 @@
+package days19;
+
+import javax.swing.JFrame;
+
+class JBorderLayout extends JFrame
+{
+	JBorderLayout()
+	{
+		
+	}
+}
+
+public class Swing07 
+{
+	public static void main(String[] args)
+	{
+		new JBorderLayout();
+	}
+}
