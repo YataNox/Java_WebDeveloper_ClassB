bf5746c100a336530c3e86d86e49eb1d998a3997
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-10 20:36:37 +0900

    Update Swing12.java

--- days19/Swing12.java
+++ days19/Swing12.java
@@ -1,11 +1,30 @@
 package days19;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 class JPanelTest extends JFrame
 {
 	JPanelTest()
 	{
+		Container con = getContentPane();
+		con.setLayout(new BorderLayout());
+		
+		JPanel  jp1 = new JPanel();
+		JPanel  jp2 = new JPanel();
+		
+		jp1.setLayout(new GridLayout(5,1));
+		jp2.setLayout(new GridLayout(4,1));
+		
+		setTitle("JPanel Test");
+		setSize(500,400);
+		setLocation(700,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
 		
 	}
 }
