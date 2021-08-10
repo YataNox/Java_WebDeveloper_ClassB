25cab63b6f595bcbc1489d54f39301da4b6606c8
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-10 17:32:27 +0900

    Update Swing04.java

--- days19/Swing04.java
+++ days19/Swing04.java
@@ -1,10 +1,53 @@
 package days19;
 
-class JImageTest extends JFrame
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+class JImageTest extends JFrame implements ActionListener
 {
+	JLabel result;
 	JImageTest()
 	{
+		// 이미지 파일을 아이콘화 해주는 클래스 ImageIcon
+		ImageIcon ii1 = new ImageIcon("images/korea.gif");
+		ImageIcon ii2 = new ImageIcon("images/germany9060.gif");
+		ImageIcon ii3 = new ImageIcon("images/usa9060.gif");
+		result = new JLabel("");
 		
+		// 버턴 이름과 이미지 아이콘으로 버튼 생성
+		JButton korea = new JButton("대한민국", ii1);
+		JButton germany = new JButton("독일", ii2);
+		JButton usa = new JButton("미국", ii3);
+		
+		// 컨테이너 설정
+		Container con = getContentPane();
+		con.setLayout(new FlowLayout());
+		con.add(korea);
+		con.add(germany);
+		con.add(usa);
+		con.add(result);
+		
+		setTitle("이미지 아이콘 실습");
+		setSize(600, 180);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		// 이벤트 설정
+		korea.addActionListener(this);
+		germany.addActionListener(this);
+		usa.addActionListener(this);
+	}
+	// 클릭 이벤트 
+	public void actionPerformed(ActionEvent e) 
+	{
+		result.setText(e.getActionCommand() + "을 선택하셨습니다.");
 	}
 }
 
