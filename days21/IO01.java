ea707c498a46b97902f04a9fd9cc99fd65a2c52c
kim <rladnwls0523@naver.com>
2021-08-12 18:49:31 +0900

    update io01

--- days21/IO01.java
+++ days21/IO01.java
@@ -25,6 +25,7 @@ public class IO01
 		// 자바의 모든 입출력 작업은 명시적으로 예외처리를 선언해야 합ㄴ디ㅏ.
 		// (IOExcetion 타입의 예외가 발생됨) : System.in.read();
 		System.out.print("정수를 입력하세요 : ");
+		// num에 12345를 입력하면 1이 들어간다 (1의 아스키 코드 값 49)
 		int num = System.in.read(); // 1바이트(한글자)읽어서 아스키코드 값으로 최종 입력됩니다.
 		// 위와 같은 바이트 입출력 클래스는 스트림으로부터 1바이트씩 읽거나 쓸 수 있습니다.
 		// 기본 입력을 제공하는 System.in 객체는 키보드와 연결된 기본 입력 스트림으로부터
@@ -32,5 +33,18 @@ public class IO01
 		// 따라서 num 변수에는 입력한 글자의 아스키코드 값이 저장됩니다. A - 65 0 - 48 a - 97
 		System.out.printf("입력된 정수 : %d\n", num);
 		System.out.printf("입력된 문자 : %c\n", (char)num);
+		// 입력 스트림의 동작 방식
+		// 입력 스트림의 경우 데이터를 입력받을 버퍼를 검색하여 해당 버퍼에 내용이 존재하지 않는 경우
+		// 버퍼에 데이터가 쌓일때까지  대기함(키보드 입력의 경우 사용자가 데이터를 입력하고 엔터키 입력할 때까지) 
+		// 연결된 버퍼에 데이터가 존재하는 경우, 해당 버퍼에서
+		// 바이트 스트림은 1byte씩 데이터를 읽어오고, 문자 스트림의 경우 2byte씩 데이터를 읽어옵니다.
+		// 기본 입력은 1바이트 읽기 실행
+		int input;
+		System.in.read();
+		System.in.read();
+		System.out.print("키보드 입력을 실행하세요 : ");
+		input = System.in.read();;
+		System.out.printf("\n입력된 값 : %d\n", input);
+		System.out.printf("입력된 문자 : %c\n", (char)input);
 	}
 }
