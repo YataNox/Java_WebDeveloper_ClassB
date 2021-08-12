aaa17c8e918bfd351bd95d841e71439719554630
YataNox <65604233+YataNox@users.noreply.github.com>
2021-08-12 17:26:48 +0900

    Create Thread02.java

---
+++ days21/Thread02.java
@@ -0,0 +1,13 @@
+package days21;
+
+// 프로그램의 수행 중 동시에 처리하고자 하는 직업이 생겼을 때는 원래 갖고 있는 메인 Thread 외
+// Thread 클래스를 추가 사용하여 실행시킬 수 있습니다.
+// Thread Class : 프로그램 실행의 흐름을 분기할 수 있는 방법을 제공하는 클래스.
+
+public class Thread02 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
