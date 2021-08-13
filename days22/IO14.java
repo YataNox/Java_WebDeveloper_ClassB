b761de3541b6b6088480243cd7b1bf4e23dccd39
USER <USER@DESKTOP-KPF14O3>
2021-08-13 17:19:26 +0900

    Update IO14.java

--- days22/IO14.java
+++ days22/IO14.java
@@ -1,10 +1,11 @@
 package days22;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class IO14 
 {
@@ -20,6 +21,7 @@ public class IO14
 			dir.mkdirs();
 		if(!dir2.exists())
 			dir2.mkdirs();
+		/*
 		File file_text = new File(dir, "abc.txt");
 		File file_text2 = new File(dir2, "abc2.txt");
 		
@@ -30,6 +32,30 @@ public class IO14
 		while((n = bis.read()) != -1)
 			bos.write((char)n);
 		bos.flush();
+		bis.close();
+		bos.close();*/
+		
+		String Filename = "eclipse-inst-jre-win64.exe";
+		String CopyPath = "C:\\Users\\USER\\Desktop\\JAVA_Programing_workspace\\copy";
+		String originPath = "C:\\Users\\USER\\Desktop";
+		
+		File originDir = new File(originPath); // 경로 설정
+		File fileOriginal = new File(originDir, Filename); // 파일 설정
+		
+		File copyDir = new File(CopyPath); // 경로 설정
+		if(!copyDir.exists())
+			copyDir.mkdirs();
+		File fileCopy = new File(copyDir, Filename); // 파일 설정
+		
+		BufferedInputStream bis = new  BufferedInputStream(new FileInputStream(fileOriginal));
+		
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(fileCopy));
+		
+		int data;
+		while((data = bis.read()) != -1)
+		{
+			bos.write(data);
+		}
 		bis.close();
 		bos.close();
 	}
