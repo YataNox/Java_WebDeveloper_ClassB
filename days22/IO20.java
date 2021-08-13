60faf93cd573b73495c05fe283bc59bf5b1cafc6
USER <USER@DESKTOP-KPF14O3>
2021-08-13 18:35:59 +0900

    update IO20.java

--- days22/IO20.java
+++ days22/IO20.java
@@ -13,13 +13,18 @@ public class IO20
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException
 	{
 		// 2021_08_13_18_28.dat
+		//파일 경로 설정 및 부재시 생성
 		File dir = new File("D:\\JAVA02\\Java_se\\temp");
 		if(!dir.exists())
 			dir.mkdirs();
+		
+		// 파일 설정
 		File file = new File(dir, "2021_08_13_18_28.dat");
+	
 		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
 		ArrayList<CalculatorResult> list = (ArrayList<CalculatorResult>)ois.readObject();
 		
+		// 파일 데이터 출력
 		for(int i = 0; i < list.size(); i++)
 		{
 			System.out.printf("%d. %s\n", i+1, list.get(i));
