66d74f0ff50b7f64d30f8bae81e2be8f540ca7fe
kim <rladnwls0523@naver.com>
2021-08-17 17:54:37 +0900

    TCPIP Update

--- days23/TcpipServer01.java
+++ days23/TcpipServer01.java
@@ -1,45 +1,50 @@
 package days23;
 
-import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TcpipServer01 
 {
 	public static void main(String[] args)
 	{
-		String serverIp = "192.168.0.92";
-		
+		ServerSocket ss = null;
+		SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
 		try 
 		{
-			// Socket : 클라이언트가 연결을 요청하면 그를 수락해줄 역할의 하드웨어 또는 소프트웨어
-			// serverIp : 현재 서버의 클라이언트가 요청할 연결의 목적지 주소
-			// 7777 : 서버에서 관리할 연결의 그룹 번호
-			//소켓이 생성되면, sc.nestInt()가 입력을 무한히 기다리듯, 연결 요청을 무한히 기다립니다.
-			Socket s = new Socket(serverIp, 7777);
-			
-			// 연결이 성공하면 아래 문장을 실행. 실패하면 catch 구문 실행
-			InputStream in = s.getInputStream(); // 소켓에서 입력/출력 스트림을 전달받아 입출력도구로 사용
-			// InputStream의 단순 아스키코드 입출력을 생성자에 넣은
-			// 서버 클라이언트 통신에 사용가능한 스트림을 생성합니다.
-			DataInputStream dis = new DataInputStream(in);
-			// 연결을 요청한 클라이언트로부터 전달된 메시지를 저장합니다.
-			String m = dis.readUTF();
-			// 소켓으로부터 받은 데이터를 출력한다.
-			System.out.println("받은 메시지 : " + m);
-			System.out.println("연결을 종료합니다.");
-			// 스트림과 소켓을 닫습니다.
-			dis.close();
-			s.close();
-			System.out.println("연결이 종료되었습니다.");
-		} catch (UnknownHostException e) 
+			ss = new ServerSocket(7777);
+			// 여기까지 완료되면 서버가 클라이언트의 연결 요청을 기다리며, 수락할 준비가 완료됩니다.
+			System.out.println("서버가 준비되었습니다.");
+			System.out.println(f.format(new Date()) + " 서버가 준비되었습니다.");
+		} catch (IOException e) 
 		{
 			e.printStackTrace();
+		} // 서버 소켓 객체 생성
+		// 서버 소켓은 준비 완료 이후 클라이언트의 연결 요청이 올 때까지 실행을 잠시 멈추고 계속 기다립니다.
+		
+		try
+		{
+			System.out.println(f.format(new Date()) + " 연결 요청을 기다립니다.");
+			Socket s = ss.accept();
+			// 클라이언트의 연결 요청이 오면 클라이언트 소켓과 통신할 새로운 통신용
+			// 소켓을 연결정보와 함께 생성하고, 서버 소켓이 연결해놓은 클라이언트 연결정보를 전달합니다.
+			System.out.println(f.format(new Date()) + s.getInetAddress() + "로부터 연결 요청이 들어왔습니다.");
+			
+			OutputStream out = s.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(out); // 최종 통신 출력 도구 : dos
+			dos.writeUTF("[공지] : 연결 성공~!! 204호 00번 서버에 접속되셨습니다.");
+			System.out.println(f.format(new Date()) + "메세지를 전송했습니다.");
+			dos.close();
+			s.close();
+			ss.close();
 		} catch (IOException e) 
 		{
 			e.printStackTrace();
 		}
+		
 	}
 }
